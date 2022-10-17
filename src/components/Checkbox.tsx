52045c62b9e2151cb2ca3fd881284daf732d178a
r-avellar <renatoavellar5@gmail.com>
2022-10-17 20:26:05 -0300

    criando interface

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,9 +1,9 @@
 import * as CheckBox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react'
 
-export interface CheckboxProps{}
+export interface CheckboxProps extends CheckBox.CheckboxProps{}
 
-export function Checkbox({}: CheckboxProps){
+export function Checkbox(props: CheckboxProps){
   return(
     <CheckBox.Root
       className="w-6 h-6 p-[2px] bg-gray-800 rounded"
